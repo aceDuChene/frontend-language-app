f2af782f2cf12ee36476bae373edb80e8ec30a92
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2022-01-19 18:27:03 -0800

    Update file.js
    ran prettier on the file

--- file.js
+++ file.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App () {
-
+export default function App() {
   return (
     <View style={styles.container}>
-        <Text style={styles.textStyle}>Fun Stuff</Text>
+      <Text style={styles.textStyle}>Fun Stuff</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   textStyle: {
     fontSize: 20,
-    color: 'blue',
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    backgroundColor: 'yellow'
-  }
-})
+    color: "blue",
+    fontWeight: "bold",
+    fontStyle: "italic",
+    backgroundColor: "yellow",
+  },
+});
