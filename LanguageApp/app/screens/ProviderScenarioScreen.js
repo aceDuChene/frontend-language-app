36cf0c3467870f0f659665a064514f064f2e9ab4
Karen Black <blackkar@oregonstate.edu>
2022-01-28 14:32:20 -0600

    added form and button functionality

--- LanguageApp/app/screens/ProviderScenarioScreen.js
+++ LanguageApp/app/screens/ProviderScenarioScreen.js
@@ -1,52 +1,90 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { Platform, StyleSheet, TextInput, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
 import ScenarioImage from "../components/ScenarioImage";
 import AppTitle from "../components/AppTitle";
 import colors from "../config/colors";
+import Screen from "../components/Screen";
 
-function ProviderScenarioScreen() {
-  const [title, setTitle] = useState("Scenario Title"); // will be set when we query DB
-  const [uriLink, setUriLink] = useState(
-    "https://reactnative.dev/img/tiny_logo.png"
-  ); // will be set when we query DB
-  const [prompt, setPrompt] = useState("Prompt Text Displayed"); // will be set when we query DB
-  const [answer, setAnswer] = useState("Answer To Prompt Displayed"); // will be set when we query DB
-  const [cpPrompt, onChangeCpPrompt] = React.useState("Type Prompt");
-  const [cpAnswer, onChangeCpAnswer] = React.useState("Type Answer");
+const initialData = {
+  id: 12312432,
+  title: "Scenario Title",
+  prompt: "What time is it?",
+  answer: "It's three in the afternoon.",
+  image: "https://reactnative.dev/img/tiny_logo.png",
+  category: "Time",
+};
+
+function ProviderScenarioScreen(translatorId) {
+  /* To be updated with scenario data from DB */
+  const [scenario, setScenario] = useState(initialData);
+
+  const [cpPrompt, setCpPrompt] = useState("");
+  const [cpAnswer, setCpAnswer] = useState("");
+
+  /* To be used with Firebase - links set after Firebase API call */
+  const [promptAudioLink, setPromptAudioLink] = useState("example link 1");
+  const [answerAudioLink, setAnswerAudioLink] = useState("example link 2");
+
+  const translatedScenario = {
+    promptTranslation: cpPrompt,
+    answerTranslation: cpAnswer,
+    promptRecording: promptAudioLink,
+    answerRecording: answerAudioLink,
+    translatorID: 11111111,
+  };
+
+  /* **** TO ADD ******
+    - API call to Firebase to get link for cpPrompt recording, setPromptAudioLink
+    - API call to Firebase to get link for cpAnswer recording, setAnswerAudioLink
+    - On Submit button press: POST request to backend API
+  */
+
+  const submitTranslation = async () => {
+    console.log("submitting to database", translatedScenario);
+    // Add code to sumbit to Firestore
+    console.log("added to firebase");
+    setCpPrompt("");
+    setCpAnswer("");
+  };
 
   return (
-    <View style={styles.container}>
-      <AppTitle style={styles.text}>{title}</AppTitle>
+    <Screen>
+      <KeyboardAwareScrollView>
+        <View style={styles.container}>
+          <AppTitle style={styles.text}>{scenario.title}</AppTitle>
 
-      <ScenarioImage uriLink={uriLink} />
+          <ScenarioImage uriLink={scenario.image} />
+          <AppText style={styles.text}>{scenario.prompt}</AppText>
+          <View style={styles.recordAnswer}></View>
+          <TextInput
+            style={styles.input}
+            placeholder="Type Prompt Translation"
+            onChangeText={(value) => setCpPrompt(value)}
+          />
+          <AppText style={styles.text}>{scenario.answer}</AppText>
+          <View style={styles.recordAnswer}></View>
+          <TextInput
+            style={styles.input}
+            placeholder="Type Answer Translation"
+            onChangeText={(value) => setCpAnswer(value)}
+          />
 
-      <AppText style={styles.text}>{prompt}</AppText>
-      <View style={styles.recordAnswer}></View>
-      <TextInput
-        style={styles.input}
-        onChangeCpPrompt={onChangeCpPrompt}
-        value={cpPrompt}
-      />
-
-      <AppText style={styles.text}>{answer}</AppText>
-      <View style={styles.recordAnswer}></View>
-      <TextInput
-        style={styles.input}
-        onChangeCpAnswer={onChangeCpAnswer}
-        value={cpAnswer}
-      />
-
-      <AppButton title="submit" />
-    </View>
+          <AppButton
+            title="submit"
+            onPress={(e) => (e.preventDefault(), submitTranslation())}
+          />
+        </View>
+      </KeyboardAwareScrollView>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
