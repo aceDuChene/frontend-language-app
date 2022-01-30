3593c9620a279f1b641d20b98c0b644559c5d040
aceDuChene <67253325+aceDuChene@users.noreply.github.com>
2022-01-29 21:47:52 -0800

    feat: record button
    creating record button component to see if it works

--- LanguageApp/App.js
+++ LanguageApp/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Screen from "./app/components/Screen";
 
+import ProviderScenarioScreen from "./app/screens/ProviderScenarioScreen";
+
 function App() {
-  return <Screen />;
+  return <ProviderScenarioScreen />;
 }
 
 export default App;
--- LanguageApp/app/screens/ProviderScenarioScreen.js
+++ LanguageApp/app/screens/ProviderScenarioScreen.js
@@ -1,52 +1,166 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { Button, StyleSheet, TextInput, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Audio } from "expo-av";
 
 import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
 import ScenarioImage from "../components/ScenarioImage";
 import AppTitle from "../components/AppTitle";
 import colors from "../config/colors";
+import Screen from "../components/Screen";
+import RecordButton from "../components/RecordButton";
 
-function ProviderScenarioScreen() {
-  const [title, setTitle] = useState("Scenario Title"); // will be set when we query DB
-  const [uriLink, setUriLink] = useState(
-    "https://reactnative.dev/img/tiny_logo.png"
-  ); // will be set when we query DB
-  const [prompt, setPrompt] = useState("Prompt Text Displayed"); // will be set when we query DB
-  const [answer, setAnswer] = useState("Answer To Prompt Displayed"); // will be set when we query DB
-  const [cpPrompt, onChangeCpPrompt] = React.useState("Type Prompt");
-  const [cpAnswer, onChangeCpAnswer] = React.useState("Type Answer");
+const initialData = {
+  id: 12312432,
+  title: "Scenario Title",
+  prompt: "What time is it?",
+  answer: "It's three in the afternoon.",
+  image: "https://reactnative.dev/img/tiny_logo.png",
+  category: "Time",
+};
+
+function ProviderScenarioScreen(translatorId) {
+  /* **** TO ADD ******
+    - API call to Firebase to post recording and get link for cpPrompt recording, setPromptAudioLink
+    - API call to Firebase to post recording get link for cpAnswer recording, setAnswerAudioLink
+    - On Submit button press: POST request to Firebase
+  */
+
+  /* To be updated with scenario data from DB */
+  const [scenario, setScenario] = useState(initialData);
+
+  const [cpPrompt, setCpPrompt] = useState("");
+  const [cpAnswer, setCpAnswer] = useState("");
+
+  /* To store Audio recordings */
+  // Stores all of recording object, (sound, uri, duration, etc..), resets to undefined in stopRecording because used in if/else
+  const [recording, setRecording] = useState();
+  const [recordedObject, setRecordedObject] = useState();
+
+  //Stores just the recording URI; change to what's needed for Firebase (sound?)
+  const [promptAudio, setPromptAudio] = useState();
+  const [answerAudio, setAnswerAudio] = useState();
+
+  /* To be used with Firebase - links set after Firebase API call */
+  const [promptAudioLink, setPromptAudioLink] = useState("example link 1");
+  const [answerAudioLink, setAnswerAudioLink] = useState("example link 2");
+
+  const translatedScenario = {
+    promptTranslation: cpPrompt,
+    answerTranslation: cpAnswer,
+    promptRecording: promptAudioLink,
+    answerRecording: answerAudioLink,
+    translatorID: 11111111,
+  };
+
+  const submitTranslation = async () => {
+    console.log("submitting to database", translatedScenario);
+    console.log("Prompt audio uri to submit to Firebase", promptAudio);
+    console.log("Answer audio uri to submit to Firebase", answerAudio);
+    /* TO DO:  Add code to sumbit to Firestore */
+    setCpPrompt("");
+    setCpAnswer("");
+  };
+
+  /* For recording audio using expo-av */
+  async function startRecording() {
+    try {
+      console.log("Requesting permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    setRecordedObject(recording);
+    console.log("Recording stopped and stored at", recording.getURI());
+  }
+
+  async function toggleRecording(itemType) {
+    console.log();
+    recording ? stopRecording : startRecording;
+    if (recording && itemType === "prompt") {
+      setPromptAudio(recordedObject);
+    }
+  }
+
+  /* For recording audio using expo-av */
+  async function startRecordingTwo() {
+    try {
+      console.log("Requesting permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecordingTwo() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    setRecordedObject(recording);
+    console.log("Recording stopped and stored at", recording.getURI());
+  }
 
   return (
-    <View style={styles.container}>
-      <AppTitle style={styles.text}>{title}</AppTitle>
+    <Screen>
+      <KeyboardAwareScrollView>
+        <View style={styles.container}>
+          <AppTitle style={styles.text}>{scenario.title}</AppTitle>
 
-      <ScenarioImage uriLink={uriLink} />
+          <ScenarioImage uriLink={scenario.image} />
+          <AppText style={styles.text}>{scenario.prompt}</AppText>
+          <RecordButton />
+          <TextInput
+            style={styles.input}
+            placeholder="Type Prompt Translation"
+            onChangeText={(value) => setCpPrompt(value)}
+          />
+          <AppText style={styles.text}>{scenario.answer}</AppText>
+          <RecordButton />
+          <TextInput
+            style={styles.input}
+            placeholder="Type Answer Translation"
+            onChangeText={(value) => setCpAnswer(value)}
+          />
 
-      <AppText style={styles.text}>{prompt}</AppText>
-      <View style={styles.recordAnswer}></View>
-      <TextInput
-        style={styles.input}
-        onChangeCpPrompt={onChangeCpPrompt}
-        value={cpPrompt}
-      />
-
-      <AppText style={styles.text}>{answer}</AppText>
-      <View style={styles.recordAnswer}></View>
-      <TextInput
-        style={styles.input}
-        onChangeCpAnswer={onChangeCpAnswer}
-        value={cpAnswer}
-      />
-
-      <AppButton title="submit" />
-    </View>
+          <AppButton
+            title="submit"
+            onPress={(e) => (e.preventDefault(), submitTranslation())}
+          />
+        </View>
+      </KeyboardAwareScrollView>
+    </Screen>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
