5be9760befd998ea8960f57baf5bb99f7c44d154
Karen Black <blackkar@oregonstate.edu>
2022-01-30 18:29:57 -0600

    Divided scenario screens into two separate screens

--- LanguageApp/app/screens/ProviderScenarioAnswerScreen.js
+++ LanguageApp/app/screens/ProviderScenarioAnswerScreen.js
@@ -7,43 +7,54 @@ import AppButton from "../components/AppButton";
 import AppText from "../components/AppText";
 import ScenarioImage from "../components/ScenarioImage";
 import AppTitle from "../components/AppTitle";
+import colors from "../config/colors";
 import Screen from "../components/Screen";
 
 const initialData = {
   id: 12312432,
-  title: "Telling Time",
+  title: "Asking the Time",
   prompt: "What time is it?",
   answer: "It's three in the afternoon.",
   image: "https://reactnative.dev/img/tiny_logo.png",
-  promptRecording: "firebaselink",
-  answerRecording: "firebaseLink",
-  promptTranslation: "Quehora es?",
-  answerTranslation: "Son las tres de la tarde",
   category: "Time",
 };
 
-function LearnerScenarioScreen(translatorId) {
+function ProviderScenarioAnswerScreen(translatorId) {
+  /* **** TO ADD ******
+    - API call to Firebase to post recording and get link for cpPrompt recording, setPromptAudioLink
+    - API call to Firebase to post recording get link for cpAnswer recording, setAnswerAudioLink
+    - On Submit button press: POST request to Firebase
+  */
+
   /* To be updated with scenario data from DB */
   const [scenario, setScenario] = useState(initialData);
-  const [cpRecording, setCpRecording] = useState();
+
+  const [cpAnswer, setCpAnswer] = useState("");
 
   /* To store Audio recordings */
   // Stores all of recording object, (sound, uri, duration, etc..), resets to undefined in stopRecording because used in if/else
   const [recording, setRecording] = useState();
-  //Stores just the recording URI; change to what's needed for Firebase
-  const [llanswerAudio, setllAnswerAudio] = useState();
 
-  //Stores LL answer text
-  const [llAnswer, setllAnswer] = useState("");
+  //Stores just the recording URI; change to what's needed for Firebase (sound?)
+  const [answerAudio, setAnswerAudio] = useState();
 
-  const gradeTranslation = async () => {
-    // If audio -> speech-to-text, update text state with setllAnswer
-    // determine distancde-wise match
-    // send alert about correct/try again
-    console.log("grading the answer");
+  /* To be used with Firebase - links set after Firebase API call */
+  const [answerAudioLink, setAnswerAudioLink] = useState("example link 2");
+
+  const translatedScenario = {
+    answerTranslation: cpAnswer,
+    answerRecording: answerAudioLink,
+    translatorID: 11111111,
   };
 
-  /* For recording audio using expo-av */
+  const submitTranslation = async () => {
+    console.log("submitting to database", translatedScenario);
+    console.log("Answer audio uri to submit to Firebase", answerAudio);
+    /* TO DO:  Add code to sumbit to Firestore */
+    setCpAnswer("");
+  };
+
+  /* For recording audio using expo-av audio functions provided in the documentation: https://docs.expo.dev/versions/latest/sdk/audio/ */
   async function startRecording() {
     try {
       console.log("Requesting permissions..");
@@ -63,35 +74,13 @@ function LearnerScenarioScreen(translatorId) {
     }
   }
 
-  async function stopRecording() {
+  async function stopRecording(item) {
     console.log("Stopping recording..");
     setRecording(undefined);
     await recording.stopAndUnloadAsync();
-    setllAnswerAudio(recording.getURI());
+    setAnswerAudio(recording.getURI());
     console.log("Recording stopped and stored at", recording.getURI());
   }
-
-  /* For playing audio expo-av audio functions provided in the documentation: https://docs.expo.dev/versions/latest/sdk/audio/*/
-  async function playSound() {
-    // load the recording based on the URI from firebase
-    console.log("Loading recording");
-    // const { cpRecording } = await Audio.Sound.createAsync({
-    //   uri: scenario.promptRecording,
-    // });
-    // setCpRecording(cpRecording);
-
-    // console.log('Playing Sound');
-    // await cpRecording.playAsync(); }
-  }
-  // Unload the recording - remove from memory
-  // React.useEffect(() => {
-  //   return cpRecording
-  //     ? () => {
-  //         console.log("Unloading Sound");
-  //         cpRecording.unloadAsync();
-  //       }
-  //     : undefined;
-  // }, [cpRecording]);
 
   return (
     <Screen>
@@ -100,35 +89,22 @@ function LearnerScenarioScreen(translatorId) {
           <AppTitle style={styles.text}>{scenario.title}</AppTitle>
 
           <ScenarioImage uriLink={scenario.image} />
-          <Button
-            style={styles.button}
-            title={"Play prompt recording"}
-            onPress={playSound}
-          />
-          <AppText style={styles.text}>{scenario.prompt}</AppText>
-          <View style={styles.spacer}></View>
+          <AppText style={styles.text}>Prompt: {scenario.prompt}</AppText>
 
+          <AppText style={styles.text}>Answer: {scenario.answer}</AppText>
           <Button
-            style={styles.button}
             title={recording ? "Stop Recording" : "Start Recording Answer"}
             onPress={recording ? stopRecording : startRecording}
           />
           <TextInput
             style={styles.input}
             placeholder="Type Answer Translation"
-            onChangeText={(value) => setllAnswer(value)}
+            onChangeText={(value) => setCpAnswer(value)}
           />
 
           <AppButton
             title="submit"
-            onPress={(e) => (e.preventDefault(), gradeTranslation())}
-          />
-
-          <AppButton
-            title="Show Answer"
-            onPress={(e) => (
-              e.preventDefault(), console.log("Show the Answer")
-            )}
+            onPress={(e) => (e.preventDefault(), submitTranslation())}
           />
         </View>
       </KeyboardAwareScrollView>
@@ -150,11 +126,10 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 10,
   },
-  button: {
-    marginBottom: 12,
-  },
-  spacer: {
-    margin: 30,
+  recordAnswer: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
   },
   text: {
     textAlign: "center",
@@ -162,4 +137,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LearnerScenarioScreen;
+export default ProviderScenarioAnswerScreen;
--- LanguageApp/app/screens/ProviderScenarioPromptScreen.js
+++ LanguageApp/app/screens/ProviderScenarioPromptScreen.js
@@ -12,14 +12,14 @@ import Screen from "../components/Screen";
 
 const initialData = {
   id: 12312432,
-  title: "Scenario Title",
+  title: "Asking the Time",
   prompt: "What time is it?",
   answer: "It's three in the afternoon.",
   image: "https://reactnative.dev/img/tiny_logo.png",
   category: "Time",
 };
 
-function ProviderScenarioScreen(translatorId) {
+function ProviderScenarioPromptScreen(translatorId) {
   /* **** TO ADD ******
     - API call to Firebase to post recording and get link for cpPrompt recording, setPromptAudioLink
     - API call to Firebase to post recording get link for cpAnswer recording, setAnswerAudioLink
@@ -30,39 +30,31 @@ function ProviderScenarioScreen(translatorId) {
   const [scenario, setScenario] = useState(initialData);
 
   const [cpPrompt, setCpPrompt] = useState("");
-  const [cpAnswer, setCpAnswer] = useState("");
 
   /* To store Audio recordings */
   // Stores all of recording object, (sound, uri, duration, etc..), resets to undefined in stopRecording because used in if/else
   const [recording, setRecording] = useState();
-  const [recordedObject, setRecordedObject] = useState();
 
   //Stores just the recording URI; change to what's needed for Firebase (sound?)
   const [promptAudio, setPromptAudio] = useState();
-  const [answerAudio, setAnswerAudio] = useState();
 
   /* To be used with Firebase - links set after Firebase API call */
   const [promptAudioLink, setPromptAudioLink] = useState("example link 1");
-  const [answerAudioLink, setAnswerAudioLink] = useState("example link 2");
 
   const translatedScenario = {
     promptTranslation: cpPrompt,
-    answerTranslation: cpAnswer,
     promptRecording: promptAudioLink,
-    answerRecording: answerAudioLink,
     translatorID: 11111111,
   };
 
   const submitTranslation = async () => {
     console.log("submitting to database", translatedScenario);
     console.log("Prompt audio uri to submit to Firebase", promptAudio);
-    console.log("Answer audio uri to submit to Firebase", answerAudio);
     /* TO DO:  Add code to sumbit to Firestore */
     setCpPrompt("");
-    setCpAnswer("");
   };
 
-  /* For recording audio using expo-av */
+  /* For recording audio using expo-av audio functions provided in the documentation: https://docs.expo.dev/versions/latest/sdk/audio/ */
   async function startRecording() {
     try {
       console.log("Requesting permissions..");
@@ -75,27 +67,19 @@ function ProviderScenarioScreen(translatorId) {
       const { recording } = await Audio.Recording.createAsync(
         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
       );
-      setRecording(recording);
+      setRecording(recording.getURI());
       console.log("Recording started");
     } catch (err) {
       console.error("Failed to start recording", err);
     }
   }
 
-  async function stopRecording() {
+  async function stopRecording(item) {
     console.log("Stopping recording..");
     setRecording(undefined);
     await recording.stopAndUnloadAsync();
-    setRecordedObject(recording);
+    setPromptAudio(recording);
     console.log("Recording stopped and stored at", recording.getURI());
-  }
-
-  async function toggleRecording(itemType) {
-    console.log();
-    recording ? stopRecording : startRecording;
-    if (recording && itemType === "prompt") {
-      setPromptAudio(recordedObject);
-    }
   }
 
   return (
@@ -105,26 +89,18 @@ function ProviderScenarioScreen(translatorId) {
           <AppTitle style={styles.text}>{scenario.title}</AppTitle>
 
           <ScenarioImage uriLink={scenario.image} />
-          <AppText style={styles.text}>{scenario.prompt}</AppText>
+          {/* <AppText style={styles.header}>"Prompt"</AppText> */}
+          <AppText style={styles.text}>Prompt: {scenario.prompt}</AppText>
           <Button
-            title={recording ? "Stop Recording" : "Start Recording"}
-            onPress={(e) => (e.preventDefault(), toggleRecording("prompt"))}
+            title={recording ? "Stop Recording" : "Start Recording Prompt"}
+            onPress={recording ? stopRecording : startRecording}
           />
           <TextInput
             style={styles.input}
             placeholder="Type Prompt Translation"
             onChangeText={(value) => setCpPrompt(value)}
           />
-          <AppText style={styles.text}>{scenario.answer}</AppText>
-          <Button
-            title={recording ? "Stop Recording" : "Start Recording"}
-            onPress={recording ? stopRecording : startRecording}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Type Answer Translation"
-            onChangeText={(value) => setCpAnswer(value)}
-          />
+          <AppText style={styles.text}>Answer: {scenario.answer}</AppText>
 
           <AppButton
             title="submit"
@@ -161,4 +137,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ProviderScenarioScreen;
+export default ProviderScenarioPromptScreen;
