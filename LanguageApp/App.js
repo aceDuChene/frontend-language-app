95e84fc8268ebaacc6eec5d5c60fb121b559e4ea
Velavee <59717974+Velavee@users.noreply.github.com>
2022-02-03 19:13:59 -0800

    fix: clean up app.js for merge

--- LanguageApp/App.js
+++ LanguageApp/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Screen from "./app/components/Screen";
-import LanguagesScreen from "./app/screens/LanguagesScreen";
 
 function App() {
-  return <LanguagesScreen />;
+  return <Screen />;
 }
 
 export default App;
