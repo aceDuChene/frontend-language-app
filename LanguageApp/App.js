43d50bc941950408083fcb4eaf8bce57b888b53b
Velavee <59717974+Velavee@users.noreply.github.com>
2022-02-03 19:04:42 -0800

    feat: add secret managment for firebase secrets

--- LanguageApp/App.js
+++ LanguageApp/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Screen from "./app/components/Screen";
+import LanguagesScreen from "./app/screens/LanguagesScreen";
 
 function App() {
-  return <Screen />;
+  return <LanguagesScreen />;
 }
 
 export default App;
