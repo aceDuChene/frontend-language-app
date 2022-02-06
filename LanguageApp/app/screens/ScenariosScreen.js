b784f0525decc20f8b3ae97aa49c9aa99cc1a300
Karen Black <blackkar@oregonstate.edu>
2022-02-06 17:58:58 -0600

    added GET call and rendering of data

--- LanguageApp/app/screens/ScenariosScreen.js
+++ LanguageApp/app/screens/ScenariosScreen.js
@@ -1,34 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
 
 import ListItem from "../components/ListItem";
 import ListItemSeparator from "../components/ListItemSeparator";
 import routes from "../navigation/routes";
+import { db } from "../../firebaseSetup";
 
-const initialScenarios = [
-  { id: 1, title: "Telling Time", prompt: "What time is it?" },
-  {
-    id: 2,
-    title: "Daily Schedule",
-    prompt: "When do you come home from school?",
-  },
-  {
-    id: 3,
-    title: "Future Events",
-    prompt: "When will you start your new job?",
-  },
-];
+// const initialScenarios = [
+//   { id: 1, title: "Telling Time", prompt: "What time is it?" },
+//   {
+//     id: 2,
+//     title: "Daily Schedule",
+//     prompt: "When do you come home from school?",
+//   },
+//   {
+//     id: 3,
+//     title: "Future Events",
+//     prompt: "When will you start your new job?",
+//   },
+// ];
 
 function ScenariosScreen({ route, navigation }) {
-  const [scenarios, setScenarios] = useState(initialScenarios);
+  const [scenarios, setScenarios] = useState();
   const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    let scenarioArray = [];
+    console.log("category selected: ", route.params.category);
+
+    db.collection("Scenarios")
+      .where("category", "==", route.params.category)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((documentSnapshot) => {
+          scenarioArray.push(documentSnapshot.data());
+        });
+        setScenarios(scenarioArray);
+        console.log("db array: ", scenarioArray);
+      });
+  }, []);
 
   return (
     <View>
       <ListItemSeparator />
       <FlatList
         data={scenarios}
-        keyExtractor={(scenario) => scenario.id.toString()}
+        keyExtractor={(scenario) => scenario.title}
         renderItem={({ item }) => (
           <ListItem
             title={item.title}
@@ -46,7 +63,7 @@ function ScenariosScreen({ route, navigation }) {
         refreshing={refreshing}
         onRefresh={() =>
           // call backend to retrieve Scenarios
-          setScenarios(initialScenarios)
+          setScenarios(scenarios)
         }
       />
     </View>
