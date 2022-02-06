2ee2b55d736db863fd70357071548e4769a44ad5
Karen Black <blackkar@oregonstate.edu>
2022-02-06 17:58:33 -0600

    added GET call and rendering of data

--- LanguageApp/app/screens/CategoriesScreen.js
+++ LanguageApp/app/screens/CategoriesScreen.js
@@ -1,22 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
 
 import ListItem from "../components/ListItem";
 import ListItemSeparator from "../components/ListItemSeparator";
 import routes from "../navigation/routes";
+import { db } from "../../firebaseSetup";
 
-const initialCategories = [
-  { name: "Time", icon: "clock" },
-  { name: "Weather", icon: "weather-partly-cloudy" },
-  { name: "Animals", icon: "dog-side" },
-  { name: "Travel", icon: "wallet-travel" },
-  { name: "Food", icon: "food" },
-  { name: "Activities", icon: "lightbulb-on-outline" },
-];
+// const initialCategories = [
+//   { name: "Time", icon: "clock" },
+//   { name: "Weather", icon: "weather-partly-cloudy" },
+//   { name: "Animals", icon: "dog-side" },
+//   { name: "Travel", icon: "wallet-travel" },
+//   { name: "Food", icon: "food" },
+//   { name: "Activities", icon: "lightbulb-on-outline" },
+// ];
 
 function CategoriesScreen({ route, navigation }) {
-  const [categories, setCategories] = useState(initialCategories);
+  const [categories, setCategories] = useState();
   const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    let categoryArray = [];
+
+    db.collection("Categories")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((documentSnapshot) => {
+          categoryArray.push(documentSnapshot.data());
+        });
+        setCategories(categoryArray);
+        console.log("db array: ", categoryArray);
+      });
+  }, []);
 
   return (
     <View>
