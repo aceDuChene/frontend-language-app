52285b7c3ef36eee69824e5f65f4cf8ad2061d8d
Karen Black <blackkar@oregonstate.edu>
2022-02-06 17:57:55 -0600

    added GET call and rendering of data

--- LanguageApp/app/screens/LanguagesScreen.js
+++ LanguageApp/app/screens/LanguagesScreen.js
@@ -1,62 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, View } from "react-native";
 import ListItem from "../components/ListItem";
 import ListItemSeparator from "../components/ListItemSeparator";
 import routes from "../navigation/routes";
 import { db } from "../../firebaseSetup";
 
-const getLanguages = async () => {
-  let languageArray = [];
-
-  await db
-    .collection("Languages")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((documentSnapshot) => {
-        languageArray.push(documentSnapshot.data());
-      });
-    });
-
-  console.log(languageArray);
-};
-
-const initialLanguages = [
-  {
-    id: 1,
-    name: "Spanish",
-    image: "https://cdn.britannica.com/36/4336-004-6BD81071/Flag-Spain.jpg",
-  },
-  {
-    id: 2,
-    name: "Japanese",
-    image:
-      "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/1-japanese-flag-with-heavy-grunge-bigalbaloo-stock.jpg",
-  },
-  {
-    id: 3,
-    name: "French",
-    image: "https://flagpedia.net/data/flags/w580/fr.png",
-  },
-];
+// const initialLanguages = [
+//   {
+//     id: 1,
+//     name: "Spanish",
+//     image: "https://cdn.britannica.com/36/4336-004-6BD81071/Flag-Spain.jpg",
+//   },
+//   {
+//     id: 2,
+//     name: "Japanese",
+//     image:
+//       "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/1-japanese-flag-with-heavy-grunge-bigalbaloo-stock.jpg",
+//   },
+//   {
+//     id: 3,
+//     name: "French",
+//     image: "https://flagpedia.net/data/flags/w580/fr.png",
+//   },
+// ];
 
 function LanguagesScreen({ route, navigation }) {
-  getLanguages();
-  const [languages, setLanguages] = useState(initialLanguages);
+  const [languages, setLanguages] = useState();
   const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    let languageArray = [];
+
+    db.collection("Languages")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((documentSnapshot) => {
+          languageArray.push(documentSnapshot.data());
+        });
+        setLanguages(languageArray);
+        console.log("db array: ", languageArray);
+      });
+  }, []);
 
   return (
     <View>
       <ListItemSeparator />
       <FlatList
         data={languages}
-        keyExtractor={(language) => language.id}
+        keyExtractor={(language) => language.englishName}
         renderItem={({ item }) => (
           <ListItem
-            title={item.name}
-            imageLink={item.image}
+            title={item.englishName}
+            imageLink={item.flag}
             onPress={() =>
               navigation.navigate(routes.CATEGORIES, {
-                language: item.name,
+                language: item.englishName,
                 user_type: route.params.user_type,
               })
             }
@@ -64,10 +62,7 @@ function LanguagesScreen({ route, navigation }) {
         )}
         ItemSeparatorComponent={ListItemSeparator}
         refreshing={refreshing}
-        onRefresh={() =>
-          // call backend to retrieve Languages
-          setLanguages(languages)
-        }
+        // onRefresh={() => getLanguages()}
       />
     </View>
   );
