b777666008b591c9273cff6658c90411168010e5
Velavee <59717974+Velavee@users.noreply.github.com>
2022-02-13 15:48:40 -0800

    feat: add validation for password fields on signup page

--- LanguageApp/app/screens/SignupScreen.js
+++ LanguageApp/app/screens/SignupScreen.js
@@ -14,13 +14,18 @@ import { auth } from "../../firebaseSetup";
 
 export default function SignupScreen({ navigation }) {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
   const [signupError, setSignupError] = useState("");
 
   const onHandleSignup = async () => {
     try {
-      if (email !== "" && password !== "") {
-        await auth.createUserWithEmailAndPassword(email, password);
+      if (email !== "" && password1 !== "" && password2 !== "") {
+        if (password1 !== password2) {
+          setSignupError("Passwords do not match.");
+        } else {
+          await auth.createUserWithEmailAndPassword(email, password2);
+        }
       }
     } catch (error) {
       setSignupError(error.message);
@@ -39,15 +44,15 @@ export default function SignupScreen({ navigation }) {
       <AppTextInput
         placeholder="Enter password"
         secureTextEntry={true}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
+        value={password1}
+        onChangeText={(text) => setPassword1(text)}
         style={LoginSignUpStyles.inputs}
       />
       <AppTextInput
         placeholder="Re-enter password"
         secureTextEntry={true}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
+        value={password2}
+        onChangeText={(text) => setPassword2(text)}
         style={LoginSignUpStyles.inputs}
       />
       <AppButton title="Submit" onPress={onHandleSignup} />
