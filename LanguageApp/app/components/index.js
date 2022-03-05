99fefbff0619075377f59c9c6dbdda379babfbe0
Velavee <59717974+Velavee@users.noreply.github.com>
2022-03-04 20:26:10 -0800

    feat: add index file for component exports

---
+++ LanguageApp/app/components/index.js
@@ -0,0 +1,37 @@
+import AppButton from "./AppButton";
+import AppButtonSecondary from "./AppButtonSecondary";
+import AppText from "./AppText";
+import AppTextInput from "./AppTextInput";
+import AppTitle from "./AppTitle";
+import AuthErrorMsg from "./AuthErrorMsg";
+import ErrorMessage from "./ErrorMessage";
+import FormMessages from "./FormMessages";
+import ListItem from "./ListItem";
+import ListItemSeparator from "./ListItemSeparator";
+import LoadingSign from "./LoadingSign";
+import MenuButton from "./MenuButton";
+import RecordButton from "./RecordButton";
+import ScenarioImage from "./ScenarioImage";
+import Screen from "./Screen";
+import SoundButton from "./SoundButton";
+import SpeechToTextButton from "./SpeechToTextButton";
+
+export {
+  AppButton,
+  AppButtonSecondary,
+  AppText,
+  AppTextInput,
+  AppTitle,
+  AuthErrorMsg,
+  ErrorMessage,
+  FormMessages,
+  ListItem,
+  ListItemSeparator,
+  LoadingSign,
+  MenuButton,
+  RecordButton,
+  ScenarioImage,
+  Screen,
+  SoundButton,
+  SpeechToTextButton,
+};
